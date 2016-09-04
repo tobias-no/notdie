c20699bbae972a48a63ac3cee285ab2a8a5f4506
Tobias Nonnenmacher <tno144@gmail.com>
2016-09-04 15:51:32 +0200

    added method to get phs of today or any other day with offset variable

--- parsexml.py
+++ parsexml.py
@@ -24,12 +24,9 @@ class XMLEx(object):
 
             from_sec = self.convert_to_sec(self.entry_dict[(entry[1].text[:19], entry[0].text)]['from'][:19], '%Y-%m-%dT%H:%M:%S')
             to_sec = self.convert_to_sec(self.entry_dict[(entry[1].text[:19], entry[0].text)]['to'][:19], '%Y-%m-%dT%H:%M:%S')
-
             self.entry_dict[(entry[1].text[:19], entry[0].text)]['from_sec'] = from_sec
             self.entry_dict[(entry[1].text[:19], entry[0].text)]['to_sec'] = to_sec
-
         self.update_diffsec()
-
 
     def update_diffsec(self):
         now_sec = float(datetime.strftime(datetime.now(), '%s.%f'))
@@ -37,15 +34,13 @@ class XMLEx(object):
             v['diff_from'] = now_sec - v['from_sec']
             v['diff_to'] = now_sec - v['to_sec']
 
-    
     def convert_to_sec(self, input_time, src_format):
         return float(datetime.strftime(datetime.strptime(input_time, src_format), '%s.%f'))
-
 
     def get_online_data(self):
         xml_file = "nodie.xml"
         for zz in range(10):
-            try:#
+            try:
                 stream = urllib2.urlopen(source_path)
                 if stream.msg == 'OK':
                     with open(xml_file, "w") as f:
@@ -58,12 +53,21 @@ class XMLEx(object):
                 if zz == 9:
                     return False
 
+    def get_phs(self, offset_h=0):
+        offset_sec = offset_h*60.*60.
+        self.update_diffsec()
+        phs = []
+        for k, v in self.entry_dict.items():
+            if (v['diff_from']+offset_sec > 0) and (v['diff_to']+offset_sec < 0):
+                phs.append(k)
+        return phs
 
 if __name__ == "__main__":
     path_to_xmlfile = "nodie.xml"
     test = XMLEx()
     #print test.get_online_data()
     test.entry_iter(path_to_xmlfile)
-    print test.entry_dict
+    #print test.entry_dict
+    print test.get_phs()
 
 
